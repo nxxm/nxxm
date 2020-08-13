5a4ea1d1d7b3cf08e8188991aa6bd841ae62da97
Marouane Amri <amri.marouane@gmail.com>
2020-08-13 13:18:23 +0200

    :wink: add nxxm install scripts

---
+++ install/install_for_macos_linux.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Linux" ]; then
+  NXXM_URL="https://github.com/nxxm/nxxm/releases/download/v0.0.8/nxxm-v0.0.8-linux-x86_64.zip"
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  NXXM_URL="https://github.com/nxxm/nxxm/releases/download/v0.0.8/nxxm-v0.0.8-macOS.zip"
+fi
+
+INSTALL_FOLDER="/usr/local"
+
+abort() {
+  printf " \e[91m $1 \n"
+  exit 1
+}
+
+info() {
+  printf "\e[1;32m ---> \e[0m $1 \n"
+}
+
+should_install_unzip() {
+  if [[ $(command -v unzip) ]]; then
+    return 1
+  fi
+}
+
+if should_install_unzip; then
+    info "unzip is needed to unzip the downloaded file, we are installing it with your package manager"
+    echo "Could you validate with your password ? 😇 "
+    sudo apt-get install unzip -y ||   abort "Error while installing unzip"
+fi
+
+info "Downloading nxxm..."
+curl -fSL $NXXM_URL --output ~/nxxm.zip || wget -q $NXXM_URL -O ~/nxxm.zip || abort "Could not download nxxm"
+info "Installing nxxm in  $INSTALL_FOLDER/bin"
+sudo unzip ~/nxxm.zip -d $INSTALL_FOLDER -x LICENSE && rm ~/nxxm.zip
+
+if [ $? -eq 0 ]; then
+    info "Nxxm successfully installed. Installing the dependencies..."
+    mkdir ~/install_nxxm && echo "#include <iostream> int main(){return 0;}">> ~/install_nxxm/installdeps.cpp
+    $INSTALL_FOLDER/bin/nxxm ~/install_nxxm
+    if [ $? -eq 0 ]; then
+        info "Nxxm and its dependencies have been successfully installed"
+    else 
+        abort "Error while installing the dependencies"
+    fi
+else
+    abort "Installation failed"
+fi
---
+++ install/install_for_windows.ps1
@@ -0,0 +1,67 @@
+﻿﻿#!/usr/bin/pwsh
+
+$INSTALL_FOLDER="C:\Windows"
+$NXXM_URL="https://github.com/nxxm/nxxm/releases/download/v0.0.8/nxxm-v0.0.8-windows-win64.zip"
+$NXXM_EXE="C:\Windows\nxxm.exe"
+$INSTALL_FORDEPS = "C:\ProgramData\install_nxxm"
+
+$texte = '#include <iostream>
+int main()
+{
+std::cout<<"Welcome in nxxm"<<std::endl;
+return 0;
+}'
+
+function Abort {
+    param (
+        $Message
+    )
+   Write-Output " $Message "
+  exit 1
+}
+
+function Info {
+    param (
+        $Message
+    )
+  Write-Output "---> $Message "
+}
+
+info "Downloading nxxm..."
+mkdir ~/nxxm
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $NXXM_URL -OutFile ~/nxxm/nxxm.zip
+
+if(!$?) {
+    Abort("Could not download nxxm")
+}
+
+info "Installing nxxm in  $INSTALL_FOLDER"
+Expand-Archive -Force -path ~/nxxm/nxxm.zip -destinationpath ~/nxxm
+Copy-Item -Force "~/nxxm/bin/nxxm.exe" -Destination $INSTALL_FOLDER
+
+if (!$?){
+    Abort("Could not install nxxm")
+}
+rm -R ~/nxxm
+
+info "Nxxm is setting up its dependencies"
+New-Item -ItemType "file" -Path $INSTALL_FORDEPS"\installdeps.cpp" -Force
+add-content $INSTALL_FORDEPS"\installdeps.cpp" $texte
+
+$command = "cmd.exe /C ""$NXXM_EXE $INSTALL_FORDEPS """
+$shell = New-Object -Com WScript.Shell
+$objExec = $shell.Exec($command)
+
+Do { 
+	$line = $objExec.StdOut.ReadLine()
+    Write-Host $line
+} while ($objExec.StdOut.AtEndOfStream -ne $true)
+   
+if ($?){
+    info "Nxxm and its dependencies have been successfully installed"
+    rm -R $INSTALL_FORDEPS
+}else{
+    rm - $INSTALL_FORDEPS
+    abort "Installation failed"
+}
